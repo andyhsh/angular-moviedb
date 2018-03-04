d1cec09cf730bc2870ec0c9a94164b02417e7202
Andy Ho <89andy@gmail.com>
2018-03-04 14:42:28 +0800

    Set up main app entry point

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,11 @@
 import angular from 'angular';
+import components from './components/components';
+import navbar from './components/navbar/navbar.component';
+import appComponent from './app.component';
+import constants from './app.constants';
+import services from './core/services';
 import 'normalize.css';
 
-angular.module('app', []);
+angular
+  .module('app', [constants, components, services])
+  .component('app', appComponent);
