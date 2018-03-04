7aa459fc476ccf07a64e217511de8010acfe10b7
Andy Ho <89andy@gmail.com>
2018-03-04 19:09:29 +0800

    Data massage function for fetching movies

--- src/app/components/navbar/navbar.controller.js
+++ src/app/components/navbar/navbar.controller.js
@@ -1,18 +1,32 @@
-export default class NavbarController {
-  constructor(moviesService) {
+export default class navbarController {
+  constructor(moviesService, API_SETTINGS) {
     'ngInject';
     this.moviesService = moviesService;
     this.moviesList = [];
-    this.data = {};
+    this.API_SETTINGS = API_SETTINGS;
   }
+
+  setJsonFormat(movieList) {
+    return movieList.map(m => {
+      const img =
+        m.poster_path !== null
+          ? `${this.API_SETTINGS.imageUrl}w92${m.poster_path}`
+          : null;
+
+      return (m = {
+        title: m.title,
+        id: m.id,
+        img
+      });
+    });
+  }
+
   handleInputChange() {
     if (this.query.length > 3) {
       this.moviesService.getMovies(this.query).then(result => {
-        this.data = result;
-        this.moviesList = result.data.results.splice(0, 8);
+        this.moviesList = this.setJsonFormat(result.data.results.splice(0, 10));
       });
     } else {
-      this.data = {};
       this.moviesList = [];
     }
   }
--- src/app/core/services.js
+++ src/app/core/services.js
@@ -3,7 +3,7 @@ import moviesService from './movies/movies.service';
 import constants from '../app.constants';
 
 const servicesModule = angular
-  .module('apps.services', [constants])
+  .module('app.services', [constants])
   .service('moviesService', moviesService).name;
 
 export default servicesModule;
