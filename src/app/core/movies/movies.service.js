5c4817bec53b3b67869a52c21a39d86ae6457a2c
Andy Ho <89andy@gmail.com>
2018-03-04 15:22:09 +0800

    Inject constants successfully

--- src/app/core/movies/movies.service.js
+++ src/app/core/movies/movies.service.js
@@ -1,19 +1,20 @@
 export default class moviesService {
-  constructor($http, constants) {
+  constructor($http, API_SETTINGS) {
     'ngInject';
     this.$http = $http;
-    this.apiPath = 'https://api.themoviedb.org/3';
-    this.apiKey = '9658ae9790af86d4edceaabc6c2c925d';
+    this.API_SETTINGS = API_SETTINGS;
   }
 
   getMovie(id) {
-    return this.$http.get(`${this.apiPath}/movie/${id}?api_key=${this.apiKey}`);
+    return this.$http.get(
+      `${this.API_SETTINGS.url}/movie/${id}?api_key=${this.API_SETTINGS.key}`
+    );
   }
 
   getMovies(query) {
     return this.$http.get(
-      `${this.apiPath}/search/movie?api_key=${
-        this.apiKey
+      `${this.API_SETTINGS.url}/search/movie?api_key=${
+        this.API_SETTINGS.key
       }&query=${query}&page=1`
     );
   }
