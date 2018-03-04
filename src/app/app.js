2eaaea64b321b2c4d6d4e9439920de5b65202e08
Andy Ho <89andy@gmail.com>
2018-03-04 20:06:20 +0800

    Add routing

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
+import uiRouter from 'angular-ui-router';
 import components from './components/components';
 import appComponent from './app.component';
 import constants from './app.constants';
@@ -6,5 +7,27 @@ import services from './core/services';
 import 'normalize.css';
 
 angular
-  .module('app', [constants, components, services])
+  .module('app', [uiRouter, constants, components, services])
+  .config(($stateProvider, $locationProvider) => {
+    'ngInject';
+    $stateProvider.state('movie', {
+      name: 'movie',
+      url: '/movie/:movieId',
+      component: 'movie',
+      resolve: {
+        movie: function(moviesService, $transition$) {
+          return moviesService
+            .getMovie($transition$.params().movieId)
+            .then(response => {
+              return response.data;
+            })
+            .catch(err => {
+              console.error('Error fetching movie');
+            });
+        }
+      }
+    });
+
+    $locationProvider.html5Mode(true);
+  })
   .component('app', appComponent);
